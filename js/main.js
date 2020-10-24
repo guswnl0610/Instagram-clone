659009553790f85707d85767ce1a3b72e6789b74
mengkki <guswnl0610@naver.com>
2020-10-24 16:09:23 +0900

    add style on navigation bar

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+//navbar-search 돋보기 이동
+
+function controll_navbar_search(){
+    const navbar_input = document.querySelector('.nav-search input');
+    const navbar_search_icon = document.querySelector('.search-icon');
+    navbar_input.addEventListener('focus',() =>{
+        navbar_search_icon.classList.add('search-focused');
+    });
+    navbar_input.addEventListener('blur', ()=>{
+        navbar_search_icon.classList.remove('search-focused');
+    });
+}
+
+function init(){
+    controll_navbar_search();
+
+}
+
+
+init();
